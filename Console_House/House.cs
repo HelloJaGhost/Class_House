3374f6bdf812ba1f3aac8b86226b0e63dea75e03
HelloJaGhost <gordeypopov@gmail.com>
2022-11-05 11:05:19 +0100

    Add class House

---
+++ Console_House/House.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Street
+{
+    public class House
+    {
+        public readonly string adress;
+        public string nickname;
+        public House(string adress, string nickname)
+        {
+            this.adress = adress;
+            this.nickname = nickname;
+        }
+        public House(string adress)
+        {
+            this.adress = adress;
+        }
+        public void Print()
+        {
+            if (nickname != null)
+                Console.WriteLine("{0}", nickname);
+            else
+            {
+                Console.WriteLine("{0}", adress);
+            }
+            Console.WriteLine();
+        }
+    }
+}
