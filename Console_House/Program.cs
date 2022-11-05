8b305c5f487381a4d757a8d4a9f6dd406b1fd2af
HelloJaGhost <gordeypopov@gmail.com>
2022-11-05 09:34:23 +0100

    Добавьте файлы проекта.

---
+++ Console_House/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Street
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        class House
+        {
+            string adress;
+            string nickname;
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Show house 1 adress");
+            Console.WriteLine("Show house 2 adress");
+            Console.WriteLine("Change house 1 name");
+            Console.WriteLine("Change house 2 name");
+        }
+    }
+}
